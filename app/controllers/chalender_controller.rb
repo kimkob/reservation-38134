cd8a6862339c57c262a51a1846015d105fe0179d
kimkob <un1ql0.go0d@gmail.com>
2022-07-25 14:31:30 +0900

    コントローラー名訂正

--- app/controllers/chalender_controller.rb
+++
@@ -1,2 +0,0 @@
-class ChalenderController < ApplicationController
-end
--- test/controllers/calendar_controller_test.rb
+++ test/controllers/calendar_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class ChalenderControllerTest < ActionDispatch::IntegrationTest
+class CalendarControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
